770a19a526a6908b806b0aeaeb2784f5f64ac5eb
hbk619 <hbk619@users.noreply.github.com>
2024-01-11 14:25:04 +0000

    feat: add a keyboard shortcut for evaluating selected text when debugging
    We need to use the Java module to cast values, this means the plugin only supports Java (for now) build.gradle.kts gives us compile access to the JavaValue and JavaValuePresentation classes inside com.intellij.java and plugin.xml gives runtime access.
    
    We are using a queue to avoid talking over ourselves and a background task to avoid this exception:
    
    java.lang.Throwable: Synchronous execution on EDT: see com.intellij.execution.process.OSProcessHandler#checkEdtAndReadAction() Javadoc for resolutions
    
    We have attempted to voice as many errors as possible! When we get an array/list/map to renderValue the valueText contains the length of the array which is useful, but if we get a normal object it's blank so use value instead which tells you what it's an instance of. It does read arrays/lists/maps twice as the first time is "collecting data" however sometimes when by the time it speaks it's been updated with the size
    
    Originally we looped through current variables but that means you can't hear on the fly stuff (e.g. typing some valid java and evaluating it) so we switched to actively evaluating the code. The value is so buried it hurts. We could use the below and skip the compute presentation but then we don't get the nice "size = 1" on arrays. Maybe we should come back to that...
    
    string:
    ((JavaValue) childValue).getDescriptor().getValue().type().name() = java.lang.string
    ((JavaValue) childValue).getDescriptor().getValue().toString() = ""yup""
    
    Other class:
    ((JavaValue) childValue).getDescriptor().getValue().type().name() = Other
    ((JavaValue) childValue).getDescriptor().getValue().toString() = instance of Other(id=762)
    
    arraylist:
    ((JavaValue) childValue).getDescriptor().getValue().type().name() = java.util.ArrayList
    ((JavaValue) childValue).getDescriptor().getValue().toString() = instance of java.util.ArrayList(id=761)
    
    int:
    ((JavaValue) childValue).getDescriptor().getValue().type().name() = int
    ((JavaValue) childValue).getDescriptor().getValue().toString() = "1"

--- src/main/kotlin/com/hbk619/jetbrainsscreenreader/ScreenReaderApp.kt
+++ src/main/kotlin/com/hbk619/jetbrainsscreenreader/ScreenReaderApp.kt
@@ -1,9 +1,15 @@
 package com.hbk619.jetbrainsscreenreader
 
 import com.intellij.ide.AppLifecycleListener
+import com.intellij.ide.DataManager
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.actionSystem.DataConstants
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.project.Project
+import com.intellij.xdebugger.XDebugProcess
+import com.intellij.xdebugger.XDebuggerManager
+
 
 class ScreenReaderApp : AppLifecycleListener {
     private val LOG: Logger = Logger.getInstance(ScreenReaderApp::class.java)
