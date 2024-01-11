4759039a98a9aa812df7728f01504bafb803811b
hbk619 <hbk619@users.noreply.github.com>
2024-01-11 14:20:37 +0000

    feat: Only beep when entering a line with a warning, not on navigating the line
    This helps avoid confusion as to whether the user needs to address the issue now or it can wait
    We switched to using the errorStripeTooltip so if squiggles aren't being shown you still get a beep

--- src/main/kotlin/com/hbk619/jetbrainsscreenreader/CaretMoveListener.kt
+++ src/main/kotlin/com/hbk619/jetbrainsscreenreader/CaretMoveListener.kt
@@ -1,17 +1,21 @@
 package com.hbk619.jetbrainsscreenreader
 
+import com.intellij.codeInsight.daemon.impl.HighlightInfo
+import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.event.CaretEvent
 import com.intellij.openapi.editor.event.CaretListener
 import com.intellij.openapi.editor.impl.DocumentMarkupModel
+import com.intellij.openapi.editor.markup.RangeHighlighter
 import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
-import com.intellij.openapi.diagnostic.Logger;
 
 class CaretMoveListener() : CaretListener {
     private val beepLock = Any()
     private val LOG: Logger = Logger.getInstance(CaretMoveListener::class.java)
+    private var previousLine = 0
 
     override fun caretPositionChanged(e: CaretEvent) {
         val caretModel = e.editor.caretModel
@@ -26,21 +30,41 @@ class CaretMoveListener() : CaretListener {
         val highlighters = markupModel.allHighlighters
 
         for (highlighter in highlighters) {
-            if (highlighter.startOffset >= startOffset && highlighter.endOffset <= endOffset && highlighter.getErrorStripeMarkColor(e.editor.colorsScheme) != null) {
+            if (highlighter.startOffset >= startOffset && highlighter.endOffset <= endOffset && highlighter.errorStripeTooltip != null) {
                 val column = caretModel.logicalPosition.column
                 val caretOffset = startOffset + column
-                if (highlighter.textRange.containsOffset(caretOffset)) {
-                    playSound("Submarine.aiff")
-                } else {
-                    playSound("Frog.aiff")
+                val tooltip = highlighter.errorStripeTooltip
+                if ( tooltip !is HighlightInfo) continue
+
+                val type = tooltip.type.getSeverity(null)
+                when (type) {
+                    HighlightSeverity.WARNING -> handleWarning(previousLine, logicalLine)
+                    HighlightSeverity.ERROR -> handleError(highlighter, caretOffset)
                 }
+
                 LOG.info("Error on line " + (logicalLine + 1))
                 break
             }
         }
+
+        previousLine = logicalLine
     }
 
-    fun playSound(soundName: String) {
+    private fun handleError(highlighter: RangeHighlighter, caretOffset: Int) {
+        if (highlighter.textRange.containsOffset(caretOffset)) {
+            playSound("Submarine.aiff")
+        } else {
+            playSound("Frog.aiff")
+        }
+    }
+
+    private fun handleWarning(previousLine: Int, logicalLine: Int) {
+        if (previousLine != logicalLine) {
+            playSound("Frog.aiff")
+        }
+    }
+
+    private fun playSound(soundName: String) {
         ApplicationManager.getApplication().executeOnPooledThread {
             synchronized(beepLock) {
                 try {
