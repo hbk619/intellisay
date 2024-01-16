4a3a2d2a9ca9984581efa89040e45bdff8b98e9c
hbk619 <hbk619@users.noreply.github.com>
2024-01-16 11:41:54 +0000

    feat: Beep on navigating to a line with a breakpoint
    We switched to a queue and blocking sound (on a background thread) to make the different beeps distinguishable

--- src/main/kotlin/com/hbk619/jetbrainsscreenreader/CaretMoveListener.kt
+++ src/main/kotlin/com/hbk619/jetbrainsscreenreader/CaretMoveListener.kt
@@ -2,20 +2,24 @@ package com.hbk619.jetbrainsscreenreader
 
 import com.intellij.codeInsight.daemon.impl.HighlightInfo
 import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.event.CaretEvent
 import com.intellij.openapi.editor.event.CaretListener
 import com.intellij.openapi.editor.impl.DocumentMarkupModel
 import com.intellij.openapi.editor.markup.RangeHighlighter
+import com.intellij.openapi.progress.BackgroundTaskQueue
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
+import com.intellij.xdebugger.impl.breakpoints.XBreakpointUtil
 import javax.sound.sampled.AudioInputStream
 import javax.sound.sampled.AudioSystem
 
 class CaretMoveListener() : CaretListener {
-    private val beepLock = Any()
     private val LOG: Logger = Logger.getInstance(CaretMoveListener::class.java)
     private var previousLine = 0
+    private val queue = BackgroundTaskQueue(null, "Playing sound")
 
     override fun caretPositionChanged(e: CaretEvent) {
         val caretModel = e.editor.caretModel
@@ -26,7 +30,6 @@ class CaretMoveListener() : CaretListener {
         val endOffset: Int = document.getLineEndOffset(logicalLine)
 
         val markupModel = DocumentMarkupModel.forDocument(document, e.editor.project, true)
-
         val highlighters = markupModel.allHighlighters
 
         for (highlighter in highlighters) {
@@ -47,8 +50,20 @@ class CaretMoveListener() : CaretListener {
                 break
             }
         }
+        handleBreakpoints(e.editor, previousLine, logicalLine)
 
         previousLine = logicalLine
+    }
+
+    private fun handleBreakpoints(editor: Editor, previousLine: Int, currentLine: Int) {
+        val project = editor.project
+        if (project != null && previousLine != currentLine) {
+            val breakpoints =
+                XBreakpointUtil.findSelectedBreakpoint(project, editor)
+            if (breakpoints.second != null) {
+                playSound("Funk.aiff")
+            }
+        }
     }
 
     private fun handleError(highlighter: RangeHighlighter, caretOffset: Int) {
@@ -66,18 +81,21 @@ class CaretMoveListener() : CaretListener {
     }
 
     private fun playSound(soundName: String) {
-        ApplicationManager.getApplication().executeOnPooledThread {
-            synchronized(beepLock) {
+        queue.run(object : Task.Backgroundable(null, "Playing sound") {
+            override fun run(progressIndicator: ProgressIndicator) {
                 try {
                     val path = CaretMoveListener::class.java.getResource("sounds/" + soundName)
                     val audioInputStream: AudioInputStream = AudioSystem.getAudioInputStream(path)
                     val clip = AudioSystem.getClip()
                     clip.open(audioInputStream)
                     clip.start()
+                    while (clip.framePosition<clip.frameLength) {
+                        LOG.debug("Running sound $soundName")
+                    }
                 } catch (e: Exception) {
                     LOG.error(e)
                 }
             }
-        }
+        })
     }
 }
