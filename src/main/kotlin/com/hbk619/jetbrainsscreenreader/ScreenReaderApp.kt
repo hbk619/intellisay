ba326f51c3ac590ea4421fabcf5fb572cc45785e
hbk619 <hbk619@users.noreply.github.com>
2024-01-18 16:35:44 +0000

    refactor: Use project listener for toggling breakpoints
    This allows us to avoid a deprecated method and listen for all types of breakpoints

--- src/main/kotlin/com/hbk619/jetbrainsscreenreader/ScreenReaderApp.kt
+++ src/main/kotlin/com/hbk619/jetbrainsscreenreader/ScreenReaderApp.kt
@@ -1,12 +1,9 @@
 package com.hbk619.jetbrainsscreenreader
 
-import com.hbk619.jetbrainsscreenreader.debugging.BreakpointsListener
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.EditorFactory
-import com.intellij.openapi.project.ProjectManager
-import com.intellij.xdebugger.XDebuggerManager
 
 
 class ScreenReaderApp : AppLifecycleListener {
@@ -20,7 +17,6 @@ class ScreenReaderApp : AppLifecycleListener {
 
         }
         EditorFactory.getInstance().eventMulticaster.addCaretListener(CaretMoveListener(), DispoableThing());
-        val proj = ProjectManager.getInstance().openProjects.find { p -> p.isOpen } ?: return
-        XDebuggerManager.getInstance(proj).breakpointManager.addBreakpointListener(BreakpointsListener());
+
     }
 }
