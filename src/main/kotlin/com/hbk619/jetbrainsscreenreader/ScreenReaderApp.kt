ccbdff60370adbd396910067f84393226d72bc73
hbk619 <hbk619@users.noreply.github.com>
2024-01-26 09:54:59 +0000

    feat: announce the plugin is ready
    So people know it's working!

--- src/main/kotlin/com/hbk619/jetbrainsscreenreader/ScreenReaderApp.kt
+++ src/main/kotlin/com/hbk619/jetbrainsscreenreader/ScreenReaderApp.kt
@@ -1,13 +1,16 @@
 package com.hbk619.jetbrainsscreenreader
 
+import com.hbk619.jetbrainsscreenreader.sound.Say
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.progress.BackgroundTaskQueue
 
 
 class ScreenReaderApp : AppLifecycleListener {
     private val LOG: Logger = Logger.getInstance(ScreenReaderApp::class.java)
+    private val queue = BackgroundTaskQueue(null, "Intelli Say started")
 
     override fun appStarted() {
         class DispoableThing : Disposable {
@@ -18,5 +21,7 @@ class ScreenReaderApp : AppLifecycleListener {
         }
         EditorFactory.getInstance().eventMulticaster.addCaretListener(CaretMoveListener(), DispoableThing());
 
+        val s = Say(null, "Intelli say started", "Intelli Say has started")
+        queue.run(s)
     }
 }
