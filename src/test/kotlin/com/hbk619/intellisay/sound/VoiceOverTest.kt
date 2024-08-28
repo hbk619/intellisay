cce2d599a4241ba1a430776ec6380e592b182a41
hbk619 <hbk619@users.noreply.github.com>
2024-08-28 10:26:48 +0100

    chore(tests): fix voiceover tests
    A logic change broke the tests!
    Added some extra tests cases to cover more scenarios for more reliability

--- src/test/kotlin/com/hbk619/intellisay/sound/VoiceOverTest.kt
+++ src/test/kotlin/com/hbk619/intellisay/sound/VoiceOverTest.kt
@@ -6,11 +6,10 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.testFramework.UsefulTestCase
 
 class VoiceOverTest : BaseTestCase() {
-    fun testValidateVoiceOverEnablesWhenAppleScriptEnabled() {
+    fun testValidateVoiceOverLeavesEnabledWhenAppleScriptEnabled() {
+        AppSettingsState.instance.useVoiceOver = true
         val mockRunner = getScriptRunner()
         mockRunner.returnValue = "1"
-
-        UsefulTestCase.assertFalse(AppSettingsState.instance.useVoiceOver)
 
         validateVoiceOver()
 
@@ -23,12 +22,48 @@ class VoiceOverTest : BaseTestCase() {
         UsefulTestCase.assertEquals(Charsets.UTF_8, commandLine.charset)
         UsefulTestCase.assertTrue(AppSettingsState.instance.useVoiceOver)
     }
-
-    fun testValidateVoiceOverDisablesWhenAppleScriptDisabled() {
+    fun testValidateVoiceOverAnnouncesAndDisablesWhenAppleScriptDisabledAndVoiceOverEnabled() {
+        AppSettingsState.instance.useVoiceOver = true
         val mockRunner = getScriptRunner()
         mockRunner.returnValue = "0"
 
+        validateVoiceOver()
+
+        val calls = mockRunner.calls
+        assertEquals(1, calls.size)
+        UsefulTestCase.assertSize(1, calls)
+        val commandLine = calls[0].args[0] as GeneralCommandLine
+
+        UsefulTestCase.assertEquals("defaults read com.apple.VoiceOver4/default SCREnableAppleScript" , commandLine.commandLineString)
+        UsefulTestCase.assertEquals(Charsets.UTF_8, commandLine.charset)
         UsefulTestCase.assertFalse(AppSettingsState.instance.useVoiceOver)
+
+        val queue = getAudibleQueue()
+        UsefulTestCase.assertSize(1, queue.calls)
+        UsefulTestCase.assertEquals(listOf("Use VoiceOver is set to true but Allow VoiceOver to be controlled by Javascript is not set. Please use VoiceOver Utility to allow  VoiceOver to be controlled by Javascript", "Voiceover not configured"), queue.calls[0].args)
+    }
+
+    fun testValidateVoiceOverDisablesWhenAppleScriptDisabled() {
+        AppSettingsState.instance.useVoiceOver = true
+        val mockRunner = getScriptRunner()
+        mockRunner.returnValue = "0"
+
+        validateVoiceOver()
+
+        val calls = mockRunner.calls
+        assertEquals(1, calls.size)
+        UsefulTestCase.assertSize(1, calls)
+        val commandLine = calls[0].args[0] as GeneralCommandLine
+
+        UsefulTestCase.assertEquals("defaults read com.apple.VoiceOver4/default SCREnableAppleScript" , commandLine.commandLineString)
+        UsefulTestCase.assertEquals(Charsets.UTF_8, commandLine.charset)
+        UsefulTestCase.assertFalse(AppSettingsState.instance.useVoiceOver)
+    }
+
+    fun testValidateVoiceOverLeavesDisableDWhenAppleScriptDisabled() {
+        AppSettingsState.instance.useVoiceOver = false
+        val mockRunner = getScriptRunner()
+        mockRunner.returnValue = "0"
 
         validateVoiceOver()
 
